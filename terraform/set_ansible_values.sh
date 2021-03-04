3fd862c9b16f9480a66bf0004e6cc0d6a73d624a
Warren Pearson <warren@insightcapital.io>
2021-03-04 20:37:57 +0000

    add MySQL python module

--- terraform/set_ansible_values.sh
+++ terraform/set_ansible_values.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-echo "---" > ../ansible/roles/meetings_api/defaults/main.yml
-echo "http_port: 80" >> ../ansible/roles/meetings_api/defaults/main.yml
+echo "---" > ../ansible/roles/simple_app/defaults/main.yml
+echo "http_port: 80" >> ../ansible/roles/simple_app/defaults/main.yml
 
-terraform output | grep rds | sed -e 's/ =/:/' | sed -e 's/"//g' |sed -e 's/:3306//' |sort >> ../ansible/roles/meetings_api/defaults/main.yml
+terraform output | grep rds | sed -e 's/ =/:/' | sed -e 's/"//g' |sed -e 's/:3306//' |sort >> ../ansible/roles/simple_app/defaults/main.yml
 
 rm -f ../ansible/hosts
 
