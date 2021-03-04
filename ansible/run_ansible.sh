9109a39ab7f5e77a07520cb59193780369b27c37
Warren Pearson <warren@insightcapital.io>
2021-03-04 21:45:57 +0000

    Error if ssh key is not supplied

--- ansible/run_ansible.sh
+++ ansible/run_ansible.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
 KEY_FILE=$1
+
+if [ -z ${KEY_FILE} ]; then
+    echo "Please specify an ssh private key"
+    exit 1
+fi
+
+
 ansible-playbook -u ubuntu -b -i hosts simple_app.yml --private-key ${KEY_FILE}
